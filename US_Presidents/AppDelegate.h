c4904e474d5ecfc66b0fd8009f7b6ecba41a621d
alive <matthewschwartz@mbp>
2017-11-17 18:43:26 -0500

    Initial Commit

---
+++ US_Presidents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  US_Presidents
+//
+//  Created by alive on 11/17/17.
+//  Copyright © 2017 Matthew Schwartz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ US_Presidents/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  US_Presidents
+//
+//  Created by alive on 11/17/17.
+//  Copyright © 2017 Matthew Schwartz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
